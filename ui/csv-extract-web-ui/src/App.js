d3aae24a6ad0579834346ddc62fdddae0796ce9c
Sameer Saini <sameer.saini@outlook.com>
2020-04-15 19:03:38 +1000

    fix

--- ui/csv-extract-web-ui/src/App.js
+++ ui/csv-extract-web-ui/src/App.js
@@ -23,36 +23,4 @@ function App() {
   );
 }
 
-class WelcomeMsg extends React.Component {
-
-  constructor(props) {
-      super(props);
-
-      this.state = {
-        data : null
-      };
-    }
-
-  getUserName() {
-      var url = rootURL + "/GetUser/"
-      fetch(url)
-      .then(resp => resp.text())
-      .then((data) => { this.setState({username: data}) } )
-  }
-
-  render() {
-      
-      if (typeof this.state.username == 'undefined') {
-          this.getUserName();
-      }
-
-      return (
-          <div>
-              <h3>Welcome { this.state.username }</h3>
-          </div>
-      )
-  };
-
-}
-
 export default App;
