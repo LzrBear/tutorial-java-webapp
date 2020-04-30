34a78f0c019479b4ade139bcfe4d2e2269a05ad8
Sameer Saini <sameer.saini@outlook.com>
2020-05-01 09:22:13 +1000

    fixed import as package has been renamed

--- webservice/src/main/java/webservice/Config.java
+++ webservice/src/main/java/webservice/Config.java
@@ -17,7 +17,7 @@ import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
-import CSVXtract.csvXtractExecutor;
+import executor.csvXtractExecutor;
 
 @Path("/Config")
 public class Config {
