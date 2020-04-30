61831cb28a1d6f5501f751e9414d3a64f7fe46f3
Sameer Saini <sameer.saini@outlook.com>
2020-04-30 11:56:45 +1000

    added log file path to ui for debug purposes

--- ui/src/csvXtract/logs.js
+++ ui/src/csvXtract/logs.js
@@ -22,14 +22,27 @@ class Logs extends React.Component {
         .then((data) => { this.setState({logs: data.Logs}) } )
     }
 
+    getLogsServerFilePath() {
+        var url = rootURL + "/getLogsServerFilePath"
+        fetch(url)
+        .then(resp => resp.json() )
+        .then((data) => { this.setState({LogsServerFilePath: data.LogsServerFilePath}) } )
+    }
+
     render() {
+
+        if (typeof this.state.LogsServerFilePath == 'undefined') {
+            this.getLogsServerFilePath();
+        }
 
         return (
             <div>
                 <h3>Logs</h3>
+                <div>Log files located on server at {this.state.LogsServerFilePath}</div>
                 <button onClick={this.getLogs}>
                     Refresh
                 </button>
+                
                 <div style={{ 'white-space': 'pre-wrap' }}>
                     { this.state.logs } 
                 </div>
